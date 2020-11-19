22dbfa0286958bf9ee0ff33331e5392d78620687
Bryaan159 <62716440+Bryaan159@users.noreply.github.com>
2020-11-18 22:41:21 -0600

    wew.cpp
    sss

---
+++ wew.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int opcion;
+  cout<<"1.Cancion[]"<<endl;
+  cin>>opcion;
+  if(opcion==1)
+  {
+    cout<<"https://www.youtube.com/watch?v=vk_xq1P7vIU"<<endl;
+  }
+}
